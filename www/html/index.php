30eea6e3a9511ff567e64149982d81eaeeb3337d
Satoshi Tejima <te.sato1030@gmail.com>
2021-04-09 06:46:37 +0900

    掲示板のトップ

--- www/html/index.php
+++ www/html/index.php
@@ -13,11 +13,48 @@
         <div class='outer'>
             <div class='contents'>
                 <?php
-                    //commentがpostされている場合
-                    if(isset($_POST["comment"])){
-                        //エスケープしてから表示
-                        $comment = htmlspecialchars($_POST["color"]);
-                        print("あなたのコメントは「${comment}」です。");
+                    $dsn = $_ENV['DSN'];
+                    $user = $_ENV['POSTGRES_USER'];
+                    $pass = $_ENV['POSTGRES_PASSWORD'];
+
+                    try {
+                        $dbh = new PDO($dsn, $user, $pass);
+                    
+                        $query_result = $dbh->query('SELECT * FROM bbs ORDER BY created_at DESC');
+
+                        $dbh = null;
+                        
+                        foreach ($query_result as $row) { ?>
+                            <div class='display'>
+                                <div class='profile'>
+                                    <img src='profile_icon.png' alt='アイコン' class='icon'>
+                                    <div class='name_title'>
+                                        <li class='name'><label>名前:<?php print htmlspecialchars($row['name'])?></label></li>
+                                        <li><label>タイトル:<?php print htmlspecialchars($row['title'])?></label></li>
+                                    </div>
+                                </div>
+                                <li>
+                                    <div class='cont'>
+                                        <label>投稿内容</label>
+                                    </div>
+                                    <div class='created_at'>
+                                        <label>投稿日時:<?php print htmlspecialchars($row['created_at'])?></label>
+                                    </div>
+                                    
+                                    <p class='post_data'><?php print htmlspecialchars($row['comment'])?></p>
+                                </li>
+                                <?php
+                                    if (!$row['created_at'] === $row['updated_at']) { ?>
+                                <li><label>最終更新日時:<?php print htmlspecialchars($row['updated_at'])?></label></li>
+                                <?php 
+                                    } 
+                                ?>
+                                <li><a href='edit.php'>編集する</a></li>
+                            </div>
+                        <?php
+                        }
+                    } catch (PDOException $e) {
+                        print "<p>接続に失敗しました。</p>";
                     }
                 ?>
             </div>
