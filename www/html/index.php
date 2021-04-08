eaa960f3160422db9e21f0b0a4214ef78b9276e7
Satoshi Tejima <te.sato1030@gmail.com>
2021-04-09 03:06:33 +0900

    トップページ

--- www/html/index.php
+++ www/html/index.php
@@ -2,43 +2,28 @@
 <html>
     <head>
         <meta charset="UTF-8" />
-        <title>PHP sample</title>
+        <title>掲示板</title>
+        <link rel="stylesheet" href="default.css">
+        <link href="https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c" rel="stylesheet">
     </head>
-    <body>
-        <?php
-            //commentがpostされている場合
-            if(isset($_POST["comment"])){
-                //エスケープしてから表示
-                $comment = htmlspecialchars($_POST["color"]);
-                print("あなたのコメントは「${comment}」です。");
-            } else {
-        ?>
-            <p>コメントしてください</p>
-            <form method="POST" action="index.php">
-                <div>
-                    <label>お名前</label>
-                    <input name="name" />
-                </div>
-                <div>
-                    <label>タイトル</label>
-                    <input name="title" />
-                </div>
-                <div>
-                    <label>メールアドレス</label>
-                    <input type="email" name="mail" />
-                </div>
-                <div>
-                    <label>文字色</label>
-                    <input type="color" name="color" />
-                </div>
-                <div>
-                    <label>投稿内容</label>
-                    <textarea name="comment" minlength="5"></textarea>
-                </div>
-                <input type="submit" value="送信" />
-            </form>
-        <?php
-            }
-        ?>
+    <body class='wf-roundedmplus1c'>
+        <header>
+            <h1>掲示板</h1>
+        </header>
+        <div class='outer'>
+            <div class='contents'>
+                <?php
+                    //commentがpostされている場合
+                    if(isset($_POST["comment"])){
+                        //エスケープしてから表示
+                        $comment = htmlspecialchars($_POST["color"]);
+                        print("あなたのコメントは「${comment}」です。");
+                    }
+                ?>
+            </div>
+        </div>
+        <footer>
+            <a href='post_comment.php'>投稿する</a>
+        </footer>
     </body>
 </html>
